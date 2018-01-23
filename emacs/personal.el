58495e0c1576c19a285fd62da92c8f18e2aad6b3
Ivan Dimitrov <ivan.v.dimitrov@protonmail.com>
2018-01-23 21:10:11 +0200

    Add personel emacs config file

---
+++ emacs/personal.el
@@ -0,0 +1,9 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Personal Emacs  configuration to be used in conjunction with prelude   ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Add line numbers
+(global-linum-mode t)
+
+;; Save the buffers from previous session
+(desktop-save-mode 1)
