c57fd2fd29ab4e9eb6ef1d410b7b638f354ef7da
Ivan Dimitrov <ivan.v.dimitrov@protonmail.com>
2018-02-16 09:46:43 +0200

    Enhance personal.el

--- emacs/personal.el
+++ emacs/personal.el
@@ -10,10 +10,18 @@
 ;; Disable whitespace-mode
 (setq prelude-whitespace nil)
 
-(setq whitespace-line-column 120) ;; limit line length
+;; (setq whitespace-line-column 120) ;; limit line length
+
+;; Attempt to reload the buffers when they are edited outside emacs
+(global-auto-revert-mode t)
+
+(global-set-key [f8] 'neotree-toggle)
+
+;; (go-guru-hl-identifier-mode)
+(add-hook 'go-mode-hook #'go-guru-hl-identifier-mode)
 
 ;;install neotree
 ;;install ag.el
 
-(provide 'personal)
+ (provide 'personal)
 ;;; personal.el ends here
