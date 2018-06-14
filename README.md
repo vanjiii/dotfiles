c23988410a2f0d31f614ea265037964a4a808b22
Ivan Dimitrov <ivan.v.dimitrov@protonmail.com>
2018-06-14 13:40:25 +0300

    add usefull func to go-dev env

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 
+
 # dotfiles
 My dotfiles and scripts
 
--- emacs/personal.el
+++ emacs/personal.el
@@ -23,15 +23,71 @@
 ;; Set global env for emacs
 (add-to-list ' exec-path "/home/vanjiii/go/bin")
 
+;; emacs-go-tag configuration
+;; snakecase: BaseDomain -> base_domain
+;; camelcase: BaseDomain -> baseDomain
+;; lispcase: BaseDomain -> base-domain
+(setq go-tag-args (list "-transform" "snakecase"))
+;; check if exists go-tag and install it if necessary
+(unless (package-installed-p 'go-tag)
+  (package-refresh-contents)
+  (package-install 'go-tag))
+
+
+;; bind the sugegstion window to keybind and stops autosuggestion
+(global-set-key (kbd "C-s-SPC") 'company-complete)
+(setq company-idle-delay nil)
 
 ;; Add scopes for go guru examples
 ;; After adding GOPATH to emacs add the package
 ;; The first is the project package, the second is the packages to ignore
 ;; git.yatrusanalytics.com/yatrus/sunshine/...,-git.yatrusanalytics.com/yatrus/sunshine/vendor/...
 
+(global-set-key (kbd "C-c C-c y") 'debug-print)
+(global-set-key (kbd "C-c C-c e") 'check-error)
 
-;;install neotree
-;;install ag.el
+;; add condition if statement is not nil
+
+(defun check-error()
+  "Insert err check under marked word"
+  (interactive)
+  (move-end-of-line 1)
+  (newline-and-indent)
+  (insert "if err != nil {")
+  (newline-and-indent)
+  (insert "return err ")
+  (newline-and-indent)
+  (insert "}")
+  (indent-for-tab-command))
+
+
+(defun debug-print ()
+  "Insert Printing snippet."
+  (interactive)
+  (insert-print)
+  (insert-arg-print))
+  
+  
+
+(defun insert-print ()
+    "Insert marking print."
+  (interactive)
+  (move-end-of-line 1)
+  (newline-and-indent)
+  (insert "fmt.Print(\"=========================\")"))
+
+;; (thing-at-point 'word 'no-properties)
+
+(defun insert-arg-print ()
+  "Insert Print with debugging parameter."
+  (interactive)
+  (move-end-of-line 1)
+  (newline-and-indent)
+  (insert "fmt.Println()"))
+
+;; install neotree
+;; install ag.el
+;; install graphql-mode
 
  (provide 'personal)
-;;; personal.el ends here
+;;; personal.el ends here 
