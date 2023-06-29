e7ebcad16654e47288b749155fce2eed44473146
Ivan Dimitrov <ivan.dimitrov@sumup.com>
2023-06-29 21:19:04 +0300

    [nvim] Set some common settings

---
+++ nvim/.config/nvim/lua/prelude/set.lua
@@ -0,0 +1,23 @@
+vim.opt.cursorline = true
+
+-- set numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- show existing tabs with 4 spaces
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+-- replace tab with spaces
+-- vim.opt.expandtab = true
+
+vim.opt.wrap = false
+
+-- don't go to end of file but N rows
+vim.opt.scrolloff = 8
+
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
