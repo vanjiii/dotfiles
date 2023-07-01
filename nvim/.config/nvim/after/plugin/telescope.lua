36ca1a0a5dc6fef6e0d1f8f25435b1819f6756e3
Ivan Dimitrov <ivan.dimitrov@sumup.com>
2023-07-01 14:53:13 +0300

    [nvim] Update telescope keybindings

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,8 +1,26 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>ph', builtin.help_tags, {})
-vim.keymap.set('n', '<leader>ps', function() 
-	builtin.grep_string({ search = vim.fn.input("Grep > ")} );
+
+local opts = function(desc)
+	return {
+		desc = "Telescope: " .. desc,
+		noremap = true,
+		silent = true,
+		nowait = true,
+	}
+end
+
+vim.keymap.set('n', '<leader>pf', builtin.find_files, opts('find files'))
+vim.keymap.set('n', '<leader>pg', builtin.live_grep, opts('live grep'))
+vim.keymap.set('n', '<leader>pb', builtin.buffers, opts('list buffers'))
+vim.keymap.set('n', '<leader>ph', builtin.help_tags, opts('help tags'))
+vim.keymap.set('n', '<leader>ps', function()
+	builtin.grep_string({
+		search = vim.fn.input("Grep > "),
+		desc = 'Telescope: grep'
+	});
 end)
+
+---
+vim.keymap.set('n', '<leader>pF', builtin.grep_string, opts('grep under cursor'))
+vim.keymap.set('n', '<leader>hh', builtin.command_history, opts('history'))
+vim.keymap.set('n', '<leader>pl', builtin.quickfix, opts('quickfix list'))
