6ad9b4654adf913adc68df7a75250d9fbf7d3111
Ivan Dimitrov <ivan.dimitrov@sumup.com>
2023-10-16 20:49:02 +0300

    [nvim] Add general remap

--- nvim/.config/nvim/lua/prelude/remap.lua
+++ nvim/.config/nvim/lua/prelude/remap.lua
@@ -1,1 +1,15 @@
-vim.keymap.set("n", "<F9>", vim.cmd.noh)
+local opts = function(desc)
+	return {
+		desc = "Quickfix: " .. desc,
+		noremap = true,
+		silent = true,
+		nowait = true,
+	}
+end
+
+vim.keymap.set("n", "<F9>", vim.cmd.noh, opts('no highlight [noh]'))
+
+vim.keymap.set("n", "]q", vim.cmd.cn, opts('item next [cn]'))
+vim.keymap.set("n", "[q", vim.cmd.cp, opts('item previous [cp]'))
+vim.keymap.set("n", "<leader>qc", vim.cmd.cclose, opts('close list [cclose]'))
+vim.keymap.set("n", "<leader>qq", vim.cmd.copen, opts('open list [copen]'))
