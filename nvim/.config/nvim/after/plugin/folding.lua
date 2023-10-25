7002c29961a03461a4c6da68841450da48dd705b
Ivan Dimitrov <ivan.dimitrov@sumup.com>
2023-10-25 18:47:55 +0300

    [nvim] Update plugin settings

---
+++ nvim/.config/nvim/after/plugin/folding.lua
@@ -0,0 +1,3 @@
+vim.o.foldmethod = 'indent'
+vim.o.foldenable = false
+vim.opt.fillchars = { fold = " " }
--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -22,19 +22,19 @@ local opts = function(desc)
 	}
 end
 
-vim.keymap.set('n', '<leader><leader>', builtin.find_files, opts('find files'))
-vim.keymap.set('n', '<leader>pf', builtin.live_grep, opts('live grep'))
+vim.keymap.set('n', '<leader>pf', builtin.find_files, opts('find files'))
+vim.keymap.set('n', '<leader>ps', builtin.live_grep, opts('live grep'))
 vim.keymap.set('n', '<leader>pg', builtin.git_status, opts('grep git status'))
 vim.keymap.set('n', '<leader>pb', builtin.buffers, opts('list buffers'))
 vim.keymap.set('n', '<leader>ph', builtin.help_tags, opts('help tags'))
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({
-		search = vim.fn.input("Grep > "),
-		desc = 'Telescope: grep'
-	});
-end)
+-- vim.keymap.set('n', '<leader>ps', function()
+-- 	builtin.grep_string({
+-- 		search = vim.fn.input("Grep > "),
+-- 		desc = 'Telescope: grep'
+-- 	});
+-- end)
 
 ---
-vim.keymap.set('n', '<leader>pF', builtin.grep_string, opts('grep under cursor'))
+vim.keymap.set('n', '<leader>pS', builtin.grep_string, opts('grep under cursor'))
 vim.keymap.set('n', '<leader>hh', builtin.command_history, opts('history'))
 vim.keymap.set('n', '<leader>pl', builtin.quickfix, opts('quickfix list'))
