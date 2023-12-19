d82cf9d2292f9be5aea111d272c9949406d92930
Ivan Dimitrov <ivan.dimitrov@sumup.com>
2023-12-20 00:09:33 +0200

    [nvim] add keybinds to move lines up/down

--- nvim/.config/nvim/lua/prelude/remap.lua
+++ nvim/.config/nvim/lua/prelude/remap.lua
@@ -13,3 +13,13 @@ vim.keymap.set("n", "]q", vim.cmd.cn, opts('item next [cn]'))
 vim.keymap.set("n", "[q", vim.cmd.cp, opts('item previous [cp]'))
 vim.keymap.set("n", "<leader>qc", vim.cmd.cclose, opts('close list [cclose]'))
 vim.keymap.set("n", "<leader>qq", vim.cmd.copen, opts('open list [copen]'))
+
+-- stollen from folke
+
+-- Move Lines
+vim.keymap.set("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
+vim.keymap.set("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
+vim.keymap.set("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
+vim.keymap.set("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+vim.keymap.set("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+vim.keymap.set("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
