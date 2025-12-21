3c463f714d0aab66a992bb13df1f504f42e54ec5
Ivan Dimitrov <ivan.v.dimitrov@pm.me>
2025-12-21 10:34:09 +0200

    [nvim] set custom statusline

--- nvim/.config/nvim/lua/prelude/set.lua
+++ nvim/.config/nvim/lua/prelude/set.lua
@@ -1,6 +1,8 @@
 local set = vim.opt
 
-set.statusline = "%f%m%r%h%w %q %=%-14.(%l:%c%V%) %{v:lua.gitbranch()} %y [%{empty(&fileencoding)?'enc':&fileencoding}] %{&expandtab?'spaces:'.&tabstop:'tabs:'.&tabstop} %P "
+-- TODO: make lualine to be custom for non-editable buffers (like nvimtree)
+set.statusline =
+"%f%m%r%h%w %q %=%-14.(%l:%c%V%) %{v:lua.gitbranch()} %y [%{empty(&fileencoding)?'enc':&fileencoding}] %{&expandtab?'spaces:'.&tabstop:'tabs:'.&tabstop} %P "
 
 set.cursorline = true
 
@@ -8,12 +10,15 @@ set.cursorline = true
 set.number = true
 set.relativenumber = true
 
+--
 -- show existing tabs with 4 spaces
-set.tabstop = 4
-set.softtabstop = 4
-set.shiftwidth = 4
--- replace tab with spaces
--- set.expandtab = true
+--
+set.tabstop = 4        -- how many spaces a <Tab> counts for
+set.softtabstop = 4    -- how many spaces when you press <Tab> in insert mode
+set.shiftwidth = 4     -- how many spaces to use for autoindent
+set.expandtab = true   -- replace tab with spaces
+set.smartindent = true -- auto-indent new lines
+set.autoindent = true  -- copy indent from current line when starting a new one
 
 set.wrap = false
 
@@ -26,12 +31,15 @@ set.incsearch = true
 set.swapfile = false
 set.backup = false
 
+-- TODO: make `set.list = true` then just swap between diff listchars (this and ' ' for all except extends and precedes)
 set.listchars = {
 	eol = '↵',
 	tab = '>-',
 	space = ' ',
 	trail = '-',
-	extends = '#',
+	extends = "⟩", -- when line continues right
+	precedes = "⟨", -- when line continues left
+	nbsp = "␣", -- non-breaking space
 }
 set.list = false
 
